4851981d4b3b567ea9abb8280b0fb733e001f88c
CodeSandbox <support@codesandbox.io>
2024-12-20 15:39:50 +0000

    Sandbox snapshot commit

--- main.py
+++ main.py
@@ -57,4 +57,4 @@ def get_prediction(data: PredictionInput):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=4000)
